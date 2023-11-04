6b5b4075c931b827d63e5e8be3fa1cf2728ee6a3
sylwiakubicz <sylwiakubicz31@gmail.com>
2023-11-04 10:40:17 +0100

    Initial commit

---
+++ src/Demo.java
@@ -0,0 +1,6 @@
+public class Demo {
+    public static void main(String[] args) {
+        Object object = new Object();
+        String name = new String("Ala");
+    }
+}
---
+++ src/FamilyMember.java
@@ -0,0 +1,8 @@
+public class FamilyMember {
+    static String surname = "Nowak";
+    String name;
+    int age;
+    public static void main(String[] args) {
+
+    }
+}
